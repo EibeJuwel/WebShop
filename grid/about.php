e95dc16412479fb489e224d617294154ff80449a
EibeJuwel <47629908+EibeJuwel@users.noreply.github.com>
2019-02-14 12:07:34 +0100

    Update about.php

--- grid/about.php
+++ grid/about.php
@@ -12,26 +12,8 @@
 <div class="main">
 <p><h4>About:</h4>
 <font style="font-size:16px;">
-Sehr geehrte Damen und Herren!<br>
-Diese Website ist ein Projekt von Schülern und ist somit nicht vollständig Bug frei. Wir haben uns größte Mühe gegeben das diese Website, so gut wie möglich und Bug frei ist wenn gegeben falls Bugs oder Sonstiges auftreten melden sie sich bitte bei unserem Support wir bedanken uns an der Stelle!!!<br>
-Diese Website ist eine Auktionswebsite hier können sie ähnlich wie bei EBay, Angebote reinstellen auf welche geboten werden kann, aber auch bieten denn es geht uns darum eventuell gewisse nicht mehr gebrauchte Gegenstände zu verkaufen, für einen Preis denn sie selber aussuchen können und auf welchem sich die Gebote noch steigern können.Um diese Webseite in vollem Umfang benutzen zu können. Brauchen sie am besten die aktuellste Version ihres Browsers.<br>
-Dies hier ist keine Japanische Auktion also hier erhöht nicht der Auktionator das gebot sondern Sie. was ihnen die Möglichkeit unter anderem gibt das sie selber den Preis so niedrig halten können wie sie es möchten unter anderem ist die Website frei nach dem Motto gute Qualität zu einem niedrigen Preis.<br>
-Wir wünschen uns, dass die zur Auktion freigegebenen Gegenstände, uns zugeschickt werden, um somit mögliche Betrugsversuche auszuschließen. Die Gegenstände werden nach der Auktion von uns an ihren rechtmäßigen Besitzer verschickt und der Geldbetrag wird über uns auch an den Artikelverfasser überwiesen.<br><br>
-<font style="font-size:20px;">
-Zitate:<p></p>
-If you don't find a way to make money<br>
-while you sleep, you will work <br>
-until you die!     ~ Warren Buffet<br>
-__________________________________________________
-<p></p>
-Menschen hören nicht auf zu Spielen weil sie älter werden,<br>
-sie werden älter weil sie aufhören zu Spielen!<br>
-__________________________________________________
-<p></p>
-Träume nicht dein Leben,<br>
-sondern Lebe deinen Traum!<br>
-<p></p>
-
+Webshop<br>
+Letzte bearbeitung am 18.01.2018
 </font></p>
 </div>
 <div class="footer"><?php include "footer.php"?></div>
